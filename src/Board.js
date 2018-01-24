30e54189140b207d6d1e0bc085cf31d1cbfb9112
Nick Leung <nickleung+bitbucket@gmail.com>
2018-01-24 14:47:44 -0800

    Started work on major and minor diagonals.

--- src/Board.js
+++ src/Board.js
@@ -95,7 +95,6 @@
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
       var board = this.attributes;
-      var hasConflict = false;
       for (var i = 0; i < board.n; i++) {
         if (this.hasRowConflictAt(i)) {
           return true;
@@ -143,12 +142,30 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var board = this.attributes;
+      var count = 0;
+      for (var i = 0; i < board.n; i++) {
+        if (board[i][majorDiagonalColumnIndexAtFirstRow] === 1) {
+          count++;
+        }
+        if (count > 1) {
+          return true;
+        }
+        majorDiagonalColumnIndexAtFirstRow++;
+      }
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var board = this.attributes;
+      var hasConflict = false;
+      for (var i = 0; i < board.n; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -158,12 +175,30 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var board = this.attributes;
+      var count = 0;
+      for (var i = 0; i < board.n; i++) {
+        if (board[i][minorDiagonalColumnIndexAtFirstRow] === 1) {
+          count++;
+        }
+        if (count > 1) {
+          return true;
+        }
+        minorDiagonalColumnIndexAtFirstRow--;
+      }
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var board = this.attributes;
+      var hasConflict = false;
+      for (var i = 0; i < board.n; i++) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
